bc3e48c16b3b40203bbe7042ea65d03af3ebf6a0
Jakub-Laska <laska.rayl@gmail.com>
2026-08-15 21:59:43 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./css/index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/react-movie-lab">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
